caa591a8e2b7a5e4e291f1eb1c0faaf6dcaf0bd6
Eduardo-Rivas <edumeru46@gmail.com>
2020-07-25 22:45:25 -0300

    Creacion de Clase Departamento

---
+++ src/aplicacion/Programa.java
@@ -0,0 +1,13 @@
+package aplicacion;
+
+import model.entitis.Departamento;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		Departamento obj = new Departamento(1, "Libros");
+		System.out.println(obj);
+
+	}
+
+}
